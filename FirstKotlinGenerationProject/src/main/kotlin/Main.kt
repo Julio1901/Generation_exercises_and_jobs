76f7d5b000fa95c05274de17dfef9d688369c178
Julio Cesar <js.cesar42@gmail.com>
2021-10-22 12:18:50 -0300

    Kotlin First Class Exercises added

---
+++ FirstKotlinGenerationProject/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val firstPersonName = "Rogério"
+    var secondPersonName = "???"
+
+    println("<$secondPersonName> Opa, eae")
+    println("<$firstPersonName> De boas?")
+    println("<$secondPersonName> Suaves")
+    println("<$firstPersonName> Qual o seu nome?")
+    println("<$secondPersonName> Meu nome é Henrique")
+    secondPersonName = "Henrique"
+    println("<$firstPersonName> Opa, prazer em conhecê-lo Henrique")
+    println("<$secondPersonName> O prazer é todo meu")
+
+
+}
+
+
